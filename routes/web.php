4c575d2750ef359092c76188d1ad9fb2da30f426
ayami <ayami.design.u@gmail.com>
2026-03-27 01:05:09 +0900

    [category]一覧画面作成

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FaqController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/faqs', [FaqController::class, 'index']);
+Route::get('/categorys', [CategoryController::class, 'index']);
