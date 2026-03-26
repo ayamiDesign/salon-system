6d67e4fc7ce9e817e52c9fc98177b18d2b538aad
ayami <ayami.design.u@gmail.com>
2026-03-27 00:52:11 +0900

    [FAQ]一覧画面作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FaqController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/faqs', [FaqController::class, 'index']);
